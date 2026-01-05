542c34152e5267c26cba9ca3447392b621291872
Arjuna-Project <arjunvk1819@gmail.com>
2026-01-05 23:58:51 +0530

    Created booking-type.js

---
+++ Users/scripts/booking-type.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const cards = document.querySelectorAll(".booking-card");
+  const continueBtn = document.getElementById("continueBtn");
+
+  let selectedType = null;
+
+  cards.forEach(card => {
+    card.addEventListener("click", () => {
+      cards.forEach(c => c.classList.remove("active"));
+      card.classList.add("active");
+
+      selectedType = card.dataset.type;
+      localStorage.setItem("bookingType", selectedType);
+    });
+  });
+
+  continueBtn.addEventListener("click", () => {
+    if (!selectedType) {
+      alert("Please select a booking type");
+      return;
+    }
+
+    if (selectedType === "scheduled") {
+      window.location.href = "../pages/date&time.html";
+    } else {
+      window.location.href = "../pages/details.html";
+    }
+  });
+});
