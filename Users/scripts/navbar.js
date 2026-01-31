927f20f1b5a34f8a6602c8fac31357b6a173911c
Arjuna-Project <arjunvk1819@gmail.com>
2026-02-01 03:23:24 +0530

    Updated navbar.js

--- Users/scripts/navbar.js
+++ Users/scripts/navbar.js
@@ -1,20 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const user = JSON.parse(localStorage.getItem("user"));
+  const user = localStorage.getItem("user");
 
   const publicLinks = document.querySelectorAll(".nav-public");
   const authLinks = document.querySelectorAll(".nav-auth");
   const userMenu = document.querySelector(".user-menu");
 
-  if (user && user.user_id) {
+  if (user) {
     // ✅ LOGGED IN
-    authLinks.forEach(link => link.style.display = "inline-flex");
-    publicLinks.forEach(link => link.style.display = "inline-flex");
+    authLinks.forEach(el => el.style.display = "inline-flex");
+    publicLinks.forEach(el => el.style.display = "inline-flex");
 
     if (userMenu) userMenu.style.display = "none";
   } else {
     // ❌ LOGGED OUT
-    authLinks.forEach(link => link.style.display = "none");
-    publicLinks.forEach(link => link.style.display = "inline-flex");
+    authLinks.forEach(el => el.style.display = "none");
+    publicLinks.forEach(el => el.style.display = "inline-flex");
 
     if (userMenu) userMenu.style.display = "flex";
   }
