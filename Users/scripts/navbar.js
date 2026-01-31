19ac1a07ec392e340b894714ea574e0473d88e10
Arjuna-Project <arjunvk1819@gmail.com>
2026-02-01 03:04:47 +0530

    Created navbar.js and link in all html files

---
+++ Users/scripts/navbar.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const user = localStorage.getItem("user");
+
+  const publicLinks = document.querySelectorAll(".nav-public");
+  const authLinks = document.querySelectorAll(".nav-auth");
+  const logoutBtn = document.getElementById("logoutBtn");
+
+  if (user) {
+    publicLinks.forEach(el => el.style.display = "none");
+    authLinks.forEach(el => el.style.display = "inline-flex");
+  } else {
+    publicLinks.forEach(el => el.style.display = "inline-flex");
+    authLinks.forEach(el => el.style.display = "none");
+  }
+});
