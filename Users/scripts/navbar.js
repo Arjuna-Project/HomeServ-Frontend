f51e4a023dab880caf41c394824085f3c4e9f659
Arjuna-Project <arjunvk1819@gmail.com>
2026-02-01 03:57:07 +0530

    changed all html files

--- Users/scripts/navbar.js
+++ Users/scripts/navbar.js
@@ -5,11 +5,11 @@ document.addEventListener("DOMContentLoaded", () => {
   const authLinks = document.querySelectorAll(".nav-auth");
 
   if (user) {
-    // ✅ Logged in
+    // Logged in
     publicLinks.forEach(el => el.style.display = "none");
     authLinks.forEach(el => el.style.display = "inline-flex");
   } else {
-    // ❌ Logged out
+    // Logged out
     publicLinks.forEach(el => el.style.display = "inline-flex");
     authLinks.forEach(el => el.style.display = "none");
   }
